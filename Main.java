70663241ac45d398e3b5340848aaffe1165ba906
Oleg Karpenko <20o13k@gmail.com>
2020-09-14 23:56:28 +0500

    New person added

--- Main.java
+++ Main.java
@@ -6,7 +6,7 @@ public class Main {
 
         System.out.println("Git example");
         Person person = new Person(1, "Oleg Karpenko");
-` `
+
         Person iam = new Person(2, "I am");
     }
 
